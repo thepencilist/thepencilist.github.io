0558c580d63fe1cee44a53c25baedb64d23ec27d
Rich McNeary <rlmcneary2@gmail.com>
2017-05-12 22:10:27 -0400

    Desktop version now OK.

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 
 setTimeout(function () {
-    var e = document.getElementById("detail");
-    e.innerHTML = "rich@limnous.com";
+    var e = document.getElementsByClassName("replace");
+    for (var i = 0; i < e.length; i++) {
+        var ih = "rich";
+        ih += "@";
+        ih += "limnous.";
+        ih += "com";
+        e[i].innerHTML = ih;
+    }
 }, 200);
+
+
+window.addEventListener("scroll", function (evt) {
+    if (evt.target.scrollingElement.nodeName.toLowerCase() !== "body") {
+        return;
+    }
+
+    var m = document.getElementsByClassName("menu-container")[0];
+    if (40 <= evt.target.body.scrollTop) {
+        if (!m.classList.contains("hidden")) {
+            m.classList.add("hidden");
+        }
+    } else {
+        if (m.classList.contains("hidden")) {
+            m.classList.remove("hidden");
+        }
+    }
+});
