6911fbc5926d7265683a2a2abcbae1712b3d0acf
Rich McNeary <rlmcneary2@gmail.com>
2017-05-15 12:50:06 -0400

    Top menu is a little more useful.

--- main.js
+++ main.js
@@ -11,22 +11,42 @@ setTimeout(function () {
 }, 200);
 
 
+var _HIDDEN_CLASS = "hidden";
+var isIncrementing = true;
+var changeScrollTop = 0;
+var lastScrollTop = 0;
 window.addEventListener("scroll", function (evt) {
     var targetName = evt.target.scrollingElement.nodeName.toLowerCase();
     if (targetName !== "body" && targetName !== "html") {
         return;
     }
 
-    var target = document.getElementsByTagName(targetName)[0];
-    var st = target.scrollTop;
-    var m = document.getElementsByClassName("menu-container")[0];
-    if (40 <= st) {
-        if (!m.classList.contains("hidden")) {
-            m.classList.add("hidden");
+    var targetElement = document.getElementsByTagName(targetName)[0];
+    var currentScrollTop = targetElement.scrollTop;
+
+    var diff = currentScrollTop - lastScrollTop;
+    if (isIncrementing) {
+        if (diff < 0) {
+            isIncrementing = false;
+            changeScrollTop = lastScrollTop;
         }
     } else {
-        if (m.classList.contains("hidden")) {
-            m.classList.remove("hidden");
+        if (0 < diff) {
+            isIncrementing = true;
+            changeScrollTop = lastScrollTop;
+        }
+    }
+
+    lastScrollTop = currentScrollTop;
+
+    var m = document.getElementsByClassName("menu-container")[0];
+    if (!isIncrementing) {
+        if (100 <= changeScrollTop - currentScrollTop || currentScrollTop === 0 && m.classList.contains(_HIDDEN_CLASS)) {
+            m.classList.remove(_HIDDEN_CLASS);
+        }
+    } else {
+        if (100 <= currentScrollTop && !m.classList.contains(_HIDDEN_CLASS)) {
+            m.classList.add(_HIDDEN_CLASS);
         }
     }
 });
