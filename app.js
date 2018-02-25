24fd39609b4d3f92dc0b1faf030e5c5aa3e92f62
Rich McNeary <rlmcneary2@gmail.com>
2018-02-25 00:16:51 -0500

    Brick wall layout.

--- app.js
+++ app.js
@@ -7,15 +7,88 @@
      * @typedef ImageItem
      * @property {string} date
      * @property {string[]} description
+     * @property {ImgData} [imgData]
+     * @property {number} [pageIndex]
      * @property {string} src
      * @property {string[]} tags
      */
+
+    /**
+     * Information about an image to be displayed.
+     * @typedef ImgData
+     * @property {number} aspectRatio
+     * @property {number} height
+     * @property {number} numberOfCells
+     * @property {number} width
+     */
+
+    // var _arNineSixteen = 9 / 16; // 0.5625 P
+    var _arSixteenNine = 16 / 9; // 1.7778 L
+    var _arThreeTwo = 3 / 2; // 1.5 L
+    // var _arTwoThree = 2 / 3; // 0.6667 P
+    var _imageSeparationWidth = 0;
+    var _maxCellsPerRow = 6;
+    var _rowMaxWidth = 800;
 
     /**
      * The image collection.
      * @type {ImageItem[]}
      */
     var _images;
+
+    /**
+     * Add a row of images to the DOM.
+     * @param {{image: ImageData, img: HTMLImageElement}[]} row 
+     */
+    function addImageRowToDom(row) {
+        // First scale all of the image heights to the height of the first image.
+        var sizes = [{ height: row[0].image.imgData.height, width: row[0].image.imgData.width }];
+
+        var a = row[0].image.imgData;
+        var b;
+        for (var i = 1; i < row.length; i++) {
+            b = row[i].image.imgData;
+            sizes.push({ height: a.height, width: (a.height * b.width) / b.height });
+        }
+
+        // Find the total width of all the scaled images.
+        var nominalWidth = sizes.reduce(function (acc, x) {
+            return acc + x.width;
+        }, 0);
+
+        // Find the height that will allow all the images to fit the maximum
+        // width.
+        var totalWidth = nominalWidth - ((row.length - 1) * _imageSeparationWidth);
+        var newHeight = (a.height * _rowMaxWidth) / totalWidth;
+
+        var image;
+        for (var j = 0; j < row.length; j++) {
+            // Update each image's dimensions to fit in the brick wall layout.
+            image = row[j].image;
+            image.imgData.width = (image.imgData.width * newHeight) / image.imgData.height;
+            image.imgData.height = newHeight;
+
+            addImageToDom(image, row[j].img);
+        }
+    }
+
+    /**
+     * Add the image to the container.
+     * @param {ImageItem} image 
+     * @param {HTMLImageElement} img 
+     */
+    function addImageToDom(image, img) {
+        // console.log(image.src.split("/").pop() + " width: " + img.naturalWidth + ", height: " + img.naturalHeight);
+        // console.log(image.src.split("/").pop() + " ar: " + image.imgData.aspectRatio + ", cells: " + image.imgData.numberOfCells);
+        // console.log(image.src.split("/").pop() + " width: " + image.imgData.width + ", height: " + image.imgData.height);
+
+        img.style.height = Math.round(image.imgData.height) + "px";
+        img.style.width = Math.round(image.imgData.width) + "px";
+
+        var container = document.getElementById(image.src);
+        container.appendChild(img);
+        container.parentElement.style.display = "inline-block"; // TODO: replace with some sort of class?
+    }
 
     /**
      * Build the paragraph content of an image's content div.
@@ -51,6 +124,28 @@
     }
 
     /**
+     * Calculates how many rows each image will occupy in a row.
+     * @param {number} aspectRatio The width of the image divided by the height of the image.
+     * @returns {number} The number of cells.
+     */
+    function calculateCells(aspectRatio) {
+        // var _arNineSixteen = 9 / 16; // 0.5625 P
+        // var _arTwoThree = 2 / 3; // 0.6667 P
+        // var _arThreeTwo = 3 / 2; // 1.5 L
+        // var _arSixteenNine = 16 / 9; // 1.7778 L
+        var cells = 1; // Fills the entire row.
+        if (_arSixteenNine < aspectRatio) {
+            cells = 6;
+        } else if (_arThreeTwo < aspectRatio) {
+            cells = 3;
+        } else if (1 < aspectRatio) {
+            cells = 2.5;
+        }
+
+        return cells;
+    }
+
+    /**
      * Create the layout for images on the page.
      * @param {HTMLElement} parent
      * @param {ImageItem[]} images The images to display on this page.
@@ -64,12 +159,11 @@
             var description;
 
             description = document.createElement("div");
-            description.className = "description";
             description.id = image.src;
 
             container = document.createElement("div");
             container.style.display = "none"; // TODO: replace with some sort of class?
-            container.className = "description drawing-container";
+            container.className = "drawing-container";
             container.appendChild(description);
 
             content = document.createElement("div");
@@ -80,30 +174,25 @@
             parent.appendChild(container);
         }
 
+        // The loadImages length must be set before the createImageElement
+        // callback is invoked. loadedImages is a sparse array.
+        var loadedImages = [];
+        loadedImages.length = images.length;
+
+        var processIndex = 0;
+
         // Load the images.
         for (var j = 0; j < images.length; j++) {
+            images[j].imgData = images[j].imgData || {};
+            images[j].imgData.pageIndex = j;
+
             // Call another function to create the proper scope for working with
             // a single image object.
             createImageElement(images[j], function (image, img) {
-                // So for now we're just adding the images as they're loaded. I
-                // expect this will change once I start building the "brick
-                // wall" display and some sort of queueing will happen until a
-                // complete "row" has accumulated.
-                createImageElementCallback(image, img);
+                loadedImages[image.imgData.pageIndex] = { image: image, img: img };
+                processIndex = processRow(loadedImages, processIndex);
             });
         }
-    }
-
-    /**
-     * Add the image to the container.
-     * @param {ImageItem} image 
-     * @param {HTMLImageElement} img 
-     */
-    function createImageElementCallback(image, img) {
-        // console.log(image.src.split("/").pop() + " width: " + img.naturalWidth + ", height: " + img.naturalHeight);
-        var container = document.getElementById(image.src);
-        container.appendChild(img);
-        container.parentElement.style.display = "initial"; // TODO: replace with some sort of class?
     }
 
     /**
@@ -121,13 +210,66 @@
 
         loadHandler = function () {
             // FYI: 'this' refers to the HTMLImageElement.
+            // console.log("Image loaded: " + this.src.split("/").pop());
+
             this.removeEventListener("load", loadHandler);
+
+            var imgData = {
+                aspectRatio: this.naturalWidth / this.naturalHeight,
+                height: this.naturalHeight,
+                numberOfCells: calculateCells(this.naturalWidth / this.naturalHeight),
+                pageIndex: image.imgData.pageIndex,
+                width: this.naturalWidth
+            };
+
+            image.imgData = imgData;
+
             callback(image, this);
         };
 
         img.addEventListener("load", loadHandler);
         img.style.width = "100%";
         img.src = image.src;
+    }
+
+    /**
+     * Handle determining when a row can be created.
+     * @param {{image: ImageData, img: HTMLImageElement}[]} images 
+     * @param {number} startIndex Start adding images to rows using the item at this index.
+     * @returns {number} Start processing rows next time from this index. 
+     */
+    function processRow(images, startIndex) {
+        var nextIndex = startIndex;
+        var rowCells = 0;
+        var row = [];
+        for (var i = startIndex; i <= images.length; i++) {
+            // All the images in the array have been dealt with. If there is a
+            // partial row remaining go ahead and add ti to the DOM then return.
+            if (images.length <= i) {
+                if (0 < row.length) {
+                    addImageRowToDom(row);
+                    return;
+                }
+            }
+
+            // images is a sparse array, there might be empty elements, if so
+            // the row is not ready to be processed.
+            if (!images[i]) {
+                break;
+            }
+
+            if (_maxCellsPerRow < rowCells + images[i].image.imgData.numberOfCells) {
+                addImageRowToDom(row);
+                nextIndex += row.length;
+                row = [];
+                rowCells = 0;
+            }
+
+            row.push(images[i]);
+            rowCells += images[i].image.imgData.numberOfCells;
+        }
+
+        return nextIndex;
     }
 
     function start() {
